bbced30d6b95c74a4fbea21ab54dd2e0c851ccd5
gilissantos <gilissantos19@gmail.com>
2024-08-18 18:11:55 -0300

    adição da pasta de arquivos e método para expotar imagem em png

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import streamlit as st
 from fpdf import FPDF
+import plotly.express as px
+import config
 
 # Cache e função para carregar os dados:
 @st.cache_data
@@ -11,49 +13,40 @@ def load_data(file_csv):
 df = load_data(r'C:\Users\gilis.santos\OneDrive - Collinson Central Services Limited\Área de Trabalho\pdf_streamlit\nba_stats.csv')
 
 # Objeto PDF com métodos de formatação do tíluo e corpo do PDF que será exportado:
-class PDF(FPDF):
-    ''' Cria o template e exporta arquivo em PDF '''
-    def header(self):
-        self.set_font('Arial', 'B', 12)
-        self.cell(0, 10, 'DataFrame Exportado', 0, 1, 'C')
-
-    def format_title(self, col_names):
-        self.set_font('Arial', 'B', 12)
-        for col_name in col_names:
-            self.cell(40, 10, col_name, 1)
-        self.ln()
-
-    def format_body(self, data):
-        self.set_font('Arial', '', 12)
-        for row in data:
-            for item in row:
-                self.cell(40, 10, str(item), 1)
-            self.ln()
 
 # Criando um top 10 baseado no Ranking de pontos dos jogadores:
-top_10_points = df[['RK', 'PLAYER', 'TEAM', 'PTS', 'MIN']].sort_values(by='RK', ascending=True).head(10)
+top_10_points = df[['RK', 'PLAYER', 'TEAM', 'PTS', 'MIN', '3PM']].sort_values(by='RK', ascending=True).head(10)
 
-# Exibe o dataframe com o top 10 pontuadores:
-st.write(top_10_points)
-
-# Criando o arquivo PDF
-pdf = PDF()
-pdf.add_page()
-pdf.format_title(top_10_points.columns)
-pdf.format_body(top_10_points.values)
-arquivo_pdf = pdf.output('arquivo.pdf')
-
-st.download_button(
-    label="Salvar PDF",
-    data=arquivo_pdf,
-    file_name="Arquivo em pdf",
-    mime="arquivo salvo em PDF"
-)
 
 # Cria um gráfico de barras com o top 10 pontuadores:
-st.bar_chart(top_10_points, x='PLAYER', y='PTS')
+fig_jogador = px.bar(top_10_points,
+            x='PLAYER', 
+            y='PTS'
+             )
 
+# Atualiza as edições do gráfico:
+fig_jogador.update_layout(
+    title='Top 10 média de pontos por jogadores',
+    xaxis_title='Jogador',
+    yaxis_title='Média de pontos'
+)
 
+# plota o gráfico:
+st.plotly_chart(fig_jogador)
 
+# Salvar o gráfico como uma imagem png:
+fig_jogador.write_image(f'{config.path_save_pdf}/grafico.png')
 
+# Criando o arquivo PDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font('Helvetica', 'B', 18)
 
+#
+# Adicionar as os gráficos que foram exportados como png:
+#for graf_arq_name in graf_arq_names:
+#    pdf.ln(10)                      # Adicionando um Padding de 10px do título para a imagem do gráfico
+#    pdf.image(graf_arq_name, x=None, y=None, w=pdf.w - 20, h=0)
+
+# Exportar/salvar a página para o formato PDF
+#pdf.output(r'C:\Users\gilis.santos\OneDrive - Collinson Central Services Limited\Área de Trabalho\pdf_streamlit\teste.pdf')
